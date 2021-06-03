f82e171644f56f5602e8e72639297aecfecc60be
me <72474684+adev-exe@users.noreply.github.com>
2021-06-03 18:37:42 -0400

    End of Chapter 12

--- app/src/main/java/com/bignerdranch/android/criminalIntent/CrimeFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalIntent/CrimeFragment.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.android.criminalIntent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +12,14 @@ import android.widget.CheckBox
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainer
+import androidx.lifecycle.ViewModelProviders
+import java.util.*
 import java.util.zip.Inflater
+import androidx.lifecycle.Observer
+
+
+private const val ARG_CRIME_ID = "crime_id"
+private const val TAG = "CrimeFragment"
 
 class CrimeFragment : Fragment() {
 
@@ -20,9 +28,16 @@ class CrimeFragment : Fragment() {
     private lateinit var dateButton: Button
     private lateinit var solvedCheckBox: CheckBox
 
+    private val crimeDetailViewModel: CrimeDetailViewModel by lazy {
+        ViewModelProviders.of(this).get(CrimeDetailViewModel::class.java)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
+        val crimeId: UUID = arguments?.getSerializable(ARG_CRIME_ID) as UUID
+        crimeDetailViewModel.loadCrime(crimeId)
     }
 
     override fun onCreateView(
@@ -42,6 +57,18 @@ class CrimeFragment : Fragment() {
         }
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeDetailViewModel.crimeLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crime ->
+                crime?.let {
+                    this.crime = crime
+                    updateUI()
+                }
+            })
     }
 
     override fun onStart() {
@@ -81,4 +108,30 @@ class CrimeFragment : Fragment() {
         }
     }
 
+
+    override fun onStop() {
+        super.onStop()
+        crimeDetailViewModel.saveCrime(crime)
+    }
+
+    private fun updateUI() {
+        titleField.setText(crime.title)
+        dateButton.text = crime.date.toString()
+        solvedCheckBox.apply {
+            isChecked = crime.isSolved
+            jumpDrawablesToCurrentState()
+        }
+    }
+
+    companion object {
+
+        fun newInstance(crimeId: UUID): CrimeFragment {
+            val args = Bundle().apply {
+                putSerializable(ARG_CRIME_ID, crimeId)
+            }
+            return CrimeFragment().apply {
+                arguments = args
+            }
+        }
+    }
 }
--- app/src/main/java/com/bignerdranch/android/criminalIntent/CrimeListViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/criminalIntent/CrimeListViewModel.kt
@@ -2,7 +2,6 @@ package com.bignerdranch.android.criminalIntent
 
 import androidx.lifecycle.ViewModel
 
-
 class CrimeListViewModel : ViewModel() {
     private val crimeRepository = CrimeRepository.get()
     val crimeListLiveData = crimeRepository.getCrimes()
